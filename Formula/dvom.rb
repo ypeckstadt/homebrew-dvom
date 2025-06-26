a145e2a502b8b338f25d3a221f4476c75849961a
Peckstadt Yves <peckstadt.yves@gmail.com>
2025-06-27 07:43:28 +0900

    Update dvom.rb

--- Formula/dvom.rb
+++ Formula/dvom.rb
@@ -1,7 +1,7 @@
 class Dvom < Formula
   desc "DVOM tool"
   homepage "https://github.com/ypeckstadt/dvom"
-  url "https://github.com/ypeckstadt/dvom/archive/v1.0.0.tar.gz"
+  url "https://github.com/ypeckstadt/dvom/archive/v0.1.0.tar.gz"
   sha256 "initial_sha256_here"
   license "MIT"
 
