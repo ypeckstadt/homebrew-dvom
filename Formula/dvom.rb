def776d710e3a075b8c222ce384f4f87ace318e4
Peckstadt Yves <peckstadt.yves@gmail.com>
2025-06-26 18:00:15 +0900

    Create dvom.rb

---
+++ Formula/dvom.rb
@@ -0,0 +1,17 @@
+class Dvom < Formula
+  desc "DVOM tool"
+  homepage "https://github.com/ypeckstadt/dvom"
+  url "https://github.com/ypeckstadt/dvom/archive/v1.0.0.tar.gz"
+  sha256 "initial_sha256_here"
+  license "MIT"
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", *std_go_args(ldflags: "-s -w")
+  end
+
+  test do
+    system "#{bin}/dvom", "--version"
+  end
+end
