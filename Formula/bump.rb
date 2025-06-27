795dc90013af6c00eabbd9c7b75444954ad40d4d
Peckstadt Yves <peckstadt.yves@gmail.com>
2025-06-27 10:51:34 +0900

    Update bump to v0.3.0
    Created by https://github.com/ypeckstadt/bump/actions/runs/15916309826

--- Formula/bump.rb
+++ Formula/bump.rb
@@ -1,8 +1,8 @@
 class Dvom < Formula
   desc "Bump tool"
   homepage "https://github.com/ypeckstadt/bump"
-  url "https://github.com/ypeckstadt/bump/archive/v0.1.0.tar.gz"
-  sha256 "3241dce1b99f66b6b7910eab718ab96c200c498367a102122c11a87ea966dd60"
+  url "https://github.com/ypeckstadt/bump/archive/v0.3.0.tar.gz"
+  sha256 "502e2a4353e30f3cb3278f8cc0dcda9062f96a1d4d350a659723f23bee7510ef"
   license "MIT"
 
   depends_on "go" => :build
